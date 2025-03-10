363b38767d6a5e5d562b8e7402b1820360a99604
Cong <125168053+buiftheescoong@users.noreply.github.com>
2025-03-11 00:49:48 +0700

    add param model_name in method load_model

--- src/train/starcoder/lora_trainer_starcoder2.py
+++ src/train/starcoder/lora_trainer_starcoder2.py
@@ -2,21 +2,20 @@ import peft
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
-from src.config.config import MODEL_NAME
 from src.train.lora_trainer import LoRATrainer
 
 
 class LoRATrainer_StarCoder2(LoRATrainer):
 
     @staticmethod
-    def load_model():
+    def load_model(model_name):
         quantization_config = BitsAndBytesConfig(load_in_8bit=True)
         model = AutoModelForCausalLM.from_pretrained(
-            MODEL_NAME,
+            model_name,
             device_map="auto" if torch.cuda.is_available() else None,
             quantization_config=quantization_config
         )
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
         return model, tokenizer
 
     def add_lora(self):
